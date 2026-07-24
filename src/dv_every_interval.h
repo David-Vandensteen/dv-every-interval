bd566931e2c81c6a2967741307d464fa0f5631cd
David-Vandensteen <dvandensteen@gmail.com>
2026-07-24 21:16:13 +0200

    Rename library to dv_every_interval
    The new name more accurately reflects the library's purpose of executing a callback periodically. This commit renames all associated files, class names, and documentation.

---
+++ src/dv_every_interval.h
@@ -0,0 +1,21 @@
+#ifndef DV_EVERY_INTERVAL_H
+#define DV_EVERY_INTERVAL_H
+#include <Arduino.h>
+
+class DV_EveryInterval {
+  public:
+    explicit DV_EveryInterval();
+    explicit DV_EveryInterval(unsigned long interval);
+    explicit DV_EveryInterval(unsigned long interval, void (*callback)());
+    DV_EveryInterval &setCallback(void (*callback)());
+    DV_EveryInterval &setInterval(unsigned long interval);
+    void update();
+
+  private:
+    unsigned long _interval = 0;
+    unsigned long _nextExecutionTime = 0;
+    bool _isInitialized = false;
+    void (*_callback)() = nullptr;
+};
+
+#endif
